8dccd9d1f52b9f2f5f0b2fa3aeb98e691c841f03
fegala <31254488+fegala@users.noreply.github.com>
2017-09-04 17:41:04 -0500

    Update Promedios.py
    Corrección de errores en el eje y

--- Promedios.py
+++ Promedios.py
@@ -4,7 +4,7 @@ import pylab as pl
 # Make an array of x values
 x = [7.0, 9.0, 8.5, 8.4]                                           
 #Make an array of y values for each x value                                
-y = [1ro, 2do, 3ro, 4to]
+y = [1, 2, 3, 4]
 # use pylab to plot x and y                  
 pl.plot(x, y, 'rp')
 #Show the plot on the screen                             
