2bc504d1f644f14d6c9c73e4da94fcecebbc52fa
fegala <31254488+fegala@users.noreply.github.com>
2017-09-04 17:40:03 -0500

    Primer intento promedios

---
+++ Promedios.py
@@ -0,0 +1,11 @@
+# lineplot.py                                                   
+import numpy as np
+import pylab as pl
+# Make an array of x values
+x = [7.0, 9.0, 8.5, 8.4]                                           
+#Make an array of y values for each x value                                
+y = [1ro, 2do, 3ro, 4to]
+# use pylab to plot x and y                  
+pl.plot(x, y, 'rp')
+#Show the plot on the screen                             
+pl.savefig('promedios.png')
