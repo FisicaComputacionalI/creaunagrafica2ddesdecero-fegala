bfc8d5bd7aeae4c970c8433bf958d64a58ef80ba
fegala <31254488+fegala@users.noreply.github.com>
2017-09-04 20:57:27 -0500

    Programa para graficar promedios
    Código y captura de pantalla de la gráfica generada en repl.it/EwS1

---
+++ Promedios.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pylab as pl
+y = [7.0, 9.0, 8.5, 8.4]                               
+x = [1ro, 2do, 3ro, 4to]                
+pl.plot(x, y, 'rp')                             
+pl.savefig('promedios.png')
